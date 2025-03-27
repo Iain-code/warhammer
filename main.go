72d7cc17c4c259e20e984003c3e095d31faf9b1d
iain-code <iainvirgo1010@live.co.uk>
2025-03-27 13:29:59 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Printf("warning: assuming default configuration. .env unreadable: %v", err)
+	}
+
+	dbUrl := os.Getenv("dbUrl")
+
+}
